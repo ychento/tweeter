c1c0797df8a840baefaa1c9dee3d60a175ff3ec0
ychento <your@email.comyiyaochen@gmail.com>
2023-07-12 01:11:31 +0000

    fix the error message

--- server/index.js
+++ server/index.js
@@ -13,13 +13,6 @@ app.use(express.static("public"));
 // The in-memory database of tweets. It's a basic object with an array in it.
 const db = require("./lib/in-memory-db");
 
-// The `data-helpers` module provides an interface to the database of tweets.
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
-//
-// Because it exports a function that expects the `db` as a parameter, we can
-// require it and pass the `db` parameter immediately:
 const DataHelpers = require("./lib/data-helpers.js")(db);
 
 // Update the dates for the initial tweets (data-files/initial-tweets.json).
